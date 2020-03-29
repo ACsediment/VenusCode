ffc69c059c25d135b5da2dcdd19b6353864b29b4
ACsediment <ACsediment@users.noreply.github.com>
2020-03-29 02:59:19 +0100

    Update VenusCode.java

--- Java/VenusCode.java
+++ Java/VenusCode.java
@@ -1,15 +1,16 @@
-import java.util.ArrayList;
-import java.util.Stack;
-import java.util.LinkedList;
-
 class VenusCode{
 	private int maxColumn;
 	public static void main(String[] args) {
+		//Usage:
+		//	1. Instantiate it;
 		VenusCode test= new VenusCode();
+		//	2. Simply pass a String to its .encode() method and it will return a modified string;
 		System.out.print(test.encode(args[0]));
+		//	3. You can change the maximum column if you like;
+		test.setMaxColumn(5);
 	}
 	public VenusCode(){
-		setMaxColumn(7);//default
+		setMaxColumn(7);//default value;
 	}
 	public VenusCode(int maxColumn_new){
 		setMaxColumn(maxColumn_new);
@@ -23,22 +24,27 @@ class VenusCode{
 	public String encode(String inputString){
 		String outputString="";
 		if (inputString.length()<=maxColumn) {
-			//if maxColumn is too large for input string, cut input in half.
+			//if maxColumn is too large for input string;
 			maxColumn=inputString.length()/2+1;
+		}
+		if (maxColumn<=0) {
+			maxColumn=1; //minimum column is 1;
 		}
 		int rawCount=inputString.length()/maxColumn+1;
 
 		for (int i=0;i<rawCount;i++) {
 			for (int j=0; j<maxColumn; j++) {
-				int charIndex=i+j*rawCount;
+				//Taking out characters by given interval to form a line;
+				int charIndex=i+j*rawCount; 
 				if (charIndex<inputString.length()) {
 					char theChar=inputString.charAt(charIndex);
 					outputString+=theChar;
-					if(theChar < 127){outputString+=" ";} //additional space for half-full
-					outputString+=" ";
+					if(theChar < 127){outputString+=" ";}
+					//additional space for half-full to keep output tidy;
+					outputString+=" ";//Space between columns;
 				}
 			}
-			outputString+="\n"; //End of this line.
+			outputString+="\n"; //End of this line;
 		}
 
 		return outputString;
