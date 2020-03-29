1bc6ec22f48883aa95bcc29fb6874c421f864b86
ACsediment <ACsediment@users.noreply.github.com>
2020-03-29 02:08:18 +0100

    add copy button

--- Web/VenusCode.js
+++ Web/VenusCode.js
@@ -2,6 +2,47 @@
 document.getElementById("textarea_in").addEventListener("input", encode);
 document.getElementById("maxColumn_in").addEventListener("input", encode);
 
+//clipboard From clipboard.js
+var clipboardSnippets = new ClipboardJS('.btn',{
+    target: function(trigger) {
+        return trigger.nextElementSibling;
+    }
+});
+clipboardSnippets.on('success', function(e) {
+    showTooltip(e.trigger, 'Copied!');
+});
+clipboardSnippets.on('error', function(e) {
+    showTooltip(e.trigger, fallbackMessage(e.action));
+});
+
+//Tooltips:
+var btns = document.querySelectorAll('.btn');
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('mouseleave', clearTooltip);
+    btns[i].addEventListener('blur', clearTooltip);
+}
+function clearTooltip(e) {
+    e.currentTarget.setAttribute('class', 'btn');
+    e.currentTarget.removeAttribute('aria-label');
+}
+function showTooltip(elem, msg) {
+    elem.setAttribute('class', 'btn tooltipped tooltipped-s');
+    elem.setAttribute('aria-label', msg);
+}
+function fallbackMessage(action) {
+    var actionMsg = '';
+    var actionKey = (action === 'cut' ? 'X' : 'C');
+    if (/iPhone|iPad/i.test(navigator.userAgent)) {
+        actionMsg = 'No support :(';
+    } else if (/Mac/i.test(navigator.userAgent)) {
+        actionMsg = 'Press ⌘-' + actionKey + ' to ' + action;
+    } else {
+        actionMsg = 'Press Ctrl-' + actionKey + ' to ' + action;
+    }
+    return actionMsg;
+}
+//End Tooltips
+
 function encode(){
 	var maxColumn=7;
 	maxColumn=document.getElementById("maxColumn_in").value;
